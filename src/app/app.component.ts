41b56079b1a75d24f90efa9bbf4e00a1c6c6c98e
Achref Gallaoui <gallaoui101@gmail.com>
2026-02-12 01:15:01 +0100

    upgrade: project dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmToasterImports } from '@spartan-ng/helm/sonner';
+import { toast } from 'ngx-sonner';
 
 @Component({
 	selector: 'ic-root',
-	imports: [],
-	templateUrl: './app.component.html',
-	styleUrl: './app.component.scss',
+	imports: [HlmToasterImports, HlmButtonImports],
+	templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+	showToast() {
+		toast('Event has been created', {
+			description: 'Sunday, December 03, 2023 at 9:00 AM',
+			action: {
+				label: 'Undo',
+				onClick: () => console.log('Undo'),
+			},
+		});
+	}
+}
